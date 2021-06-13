e52a4f6b6f082118df1ded34946666da194fcd71
michaelfan0310 <michaelfan0310@gmail.com>
2021-06-12 22:00:13 -0400

    add index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+var slideShow = function(container) {
+    this.images = [];
+    this.curImage = 0;
+    for (i = 0; i < container.childElementCount; i++) {
+        this.images.push(container.children[i]);
+        this.images[i].style.display = "none";
+    }
+
+    // Handle going to to the next slide
+    var nextSlide = function() {
+        for (var i = 0; i < this.images.length; i++) {
+            this.images[i].style.display = "none";
+        }
+        this.images[this.curImage].style.display = "block";
+        this.curImage++;
+        if (this.curImage >= this.images.length) {
+            this.curImage = 0;
+        }
+        window.setTimeout(nextSlide.bind(this), 1500);
+    };
+
+    nextSlide.call(this);
+};
+ slideShow(document.getElementById("slideshow"));
