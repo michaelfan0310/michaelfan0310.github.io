3416e395ab60b8073c8483c7156e30a359a3c633
michaelfan0310 <michaelfan0310@gmail.com>
2021-08-24 00:44:02 -0400

    3ths changed

---
+++ index2.js
@@ -0,0 +1,9 @@
+// import React from "react";
+// import ReactDom from "react-dom";
+
+// ReactDom.render(
+//     <h1 className="heading">Good Morning</h1>,
+
+// document.getElementById("root")
+
+// );
