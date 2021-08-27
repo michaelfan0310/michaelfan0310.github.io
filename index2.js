3664ce82e09d62b3e1f115dc6fb1fe70c882a952
michaelfan0310 <michaelfan0310@gmail.com>
2021-08-27 13:57:08 -0400

    js fixed

--- index2.js
+++
@@ -1,9 +0,0 @@
-// import React from "react";
-// import ReactDom from "react-dom";
-
-// ReactDom.render(
-//     <h1 className="heading">Good Morning</h1>,
-
-// document.getElementById("root")
-
-// );
