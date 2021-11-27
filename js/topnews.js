982edbece5cd16f1bda5dd2ee38a0306ceed757b
michaelfan0310 <michaelfan0310@gmail.com>
2021-11-26 20:27:52 -0500

    Update topnews.js

--- js/topnews.js
+++ js/topnews.js
@@ -4,18 +4,20 @@ const api_url_news='https://newsapi.org/v2/top-headlines?sources=techcrunch&apiK
 
 async function getNews(){
     const response= await fetch(api_url_news);
-    const data= await response.json();     
-          
+    const data= await response.json();  
+
+     console.log(data.articles[0].title);
+    console.log(data.articles[0].description);     
 
     console.log(data.articles[1].title);
-    // console.log(data.articles[1].description);
+    console.log(data.articles[1].description);
     // console.log(data.main.temp);
     
-    document.getElementById('newstitle1').textContent=data.articles[0].title;
-    document.getElementById('newstext1').textContent=data.articles[0].description;
+    // document.getElementById('newstitle1').textContent=data.articles[0].title;
+    // document.getElementById('newstext1').textContent=data.articles[0].description;
 
-    document.getElementById('newstitle2').textContent=data.articles[1].title;
-    document.getElementById('newstext2').textContent=data.articles[1].description;
+    // document.getElementById('newstitle2').textContent=data.articles[1].title;
+    // document.getElementById('newstext2').textContent=data.articles[1].description;
 
     document.getElementById('newstitle3').textContent=data.articles[2].title;
     document.getElementById('newstext3').textContent=data.articles[2].description;
