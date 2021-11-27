21c408994e3ad12a01cae62c6ce5fd105e840596
michaelfan0310 <michaelfan0310@gmail.com>
2021-11-26 21:50:25 -0500

    updated

--- js/topnews.js
+++ js/topnews.js
@@ -1,11 +1,16 @@
 const api_url_news='https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=294749526c424fe6bb4feb6f396b4372';
 
+const api_url_media='http://api.mediastack.com/v1/news?access_key=c4f081a4b0b89eeeedaae32d42f340f6&countries=us,au&sources=en,-fr';
 
 
 async function getNews(){
     // const response= await fetch(api_url_news);
     // const data= await response.json();  
+    const response= await fetch(api_url_media);
+    const media_data= await response.json(); 
 
+    console.log(media_data.data[0].title);
+    console.log(media_data.data[0].description);
     //  console.log(data.articles[0].title);
     // console.log(data.articles[0].description);     
 
@@ -19,8 +24,8 @@ async function getNews(){
     // document.getElementById('newstitle2').textContent=data.articles[1].title;
     // document.getElementById('newstext2').textContent=data.articles[1].description;
 
-    document.getElementById('newstitle3').textContent=data.articles[2].title;
-    document.getElementById('newstext3').textContent=data.articles[2].description;
+    document.getElementById('newstitle3').textContent=media_data.data[0].title;
+    document.getElementById('newstext3').textContent=media_data.data[0].description;
     
 }
 
