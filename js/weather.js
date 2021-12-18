7de9aa8ddbb8d748db01bca0d41a022e7950ccb5
michaelfan0310 <michaelfan0310@gmail.com>
2021-12-18 11:37:04 -0500

    updated weather

--- js/weather.js
+++ js/weather.js
@@ -22,7 +22,7 @@ if ('geolocation' in navigator){
 }
 
 // <!-- '+lat+'-->
-    const api_url='https://api.openweathermap.org/data/2.5/weather?q=london,Ontaria&appid='+apiKEY+'&units=metric';
+    const api_url='https://api.openweathermap.org/data/2.5/weather?q=Woodstock,Ontario&appid=6576f538d91956037524f524f1a31cb8&units=metric';
 
     // const api_url_geo='https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long+'&appid='+apiKEY+'&units=metric';
     const response= await fetch(api_url);
@@ -33,7 +33,7 @@ if ('geolocation' in navigator){
     const img=document.querySelector('#weatherIcon');
     
     img.src=imageURL;
-    console.log(response);
+    // console.log(response);
     // img.style.width="2px";
     // document.body.appendChild(img);   
 
@@ -50,7 +50,7 @@ if ('geolocation' in navigator){
 }
 
 getWeather();
-setInterval(getWeather,90000);
+setInterval(getWeather,300000);
 
 
 
