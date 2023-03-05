b7ba22642e42685a194066db5f58c7f1e9aebe50
marshall2023 <74441675+michaelfan0310@users.noreply.github.com>
2023-03-05 18:30:41 -0500

    3-5-1st

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 $(function () {
      showhide()
+     search()
 
      function showhide () {
         $('[name=show_hide]').hover(function () { // 显示
@@ -9,5 +10,18 @@ $(function () {
           var id = this.id + '_items'
           $('#'+id).hide()
         })
+      }
+
+      function search() {
+        $('#txtSearch')
+          .on('foucs keyup',function () {
+            var text = this.value.trim()
+            if (text){
+              $('#search_helper').show()
+            }
+          })
+          .blur(function () {
+            $('#search_helper').hide()
+          })
       }
 })
